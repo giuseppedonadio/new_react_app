0947b8b7bae49d2fb6bedfa20ae8f1f5b956273d
giuseppedonadio <gdonad01@seattlecentral.edu>
2018-05-22 20:17:18 -0700

    JS Array

---
+++ public/js/seed.js
@@ -0,0 +1,22 @@
+window.Seed = (function () {
+
+  const projects = [
+    {
+      id: 1,
+      title: 'xxxxxxxxx',
+      description: 'xxxxxxxxx',
+      url: '#',
+      projectImageUrl: 'xxxxxxxxx',
+    },
+    {
+      id: 2,
+      title: 'xxxxxxxxx',
+      description: 'xxxxxxxxx',
+      url: '#',
+      projectImageUrl: 'xxxxxxxxx',
+    },
+
+  ];
+
+  return { projects: projects };
+}());
